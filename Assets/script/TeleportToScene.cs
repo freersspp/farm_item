cff98f30643e926680b2d05eabc2b6f4d088c58e
freersspp <freersspp@gmail.com>
2025-07-24 01:25:54 +0800

    Update_場景修改

---
+++ Assets/script/TeleportToScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportToScene : MonoBehaviour
+{
+    public string sceneName; // 要傳送到的場景名稱
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) // 確保只有主角觸發
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
